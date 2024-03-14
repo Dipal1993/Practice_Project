23bf9db60c433841298659c4e10f09cd785b164a
your name <soft.testing1993@gmail.com>
2024-03-14 17:25:35 +0000

    Revert "projectfile12"
    This reverts commit 0f3c890e5f9e1b51f918d9182c3845bee7eff97d.

--- src/project.java
+++ src/project.java
@@ -3,6 +3,5 @@ public class project
     public static void main(String[] args)
     {
         System.out.println("My Practice_Project");
-        System.out.println("My Practice_Project1");
     }
 }
